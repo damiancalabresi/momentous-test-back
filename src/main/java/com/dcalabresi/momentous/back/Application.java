9b03d7163a181c76132d78c70d72e1008c687fc5
Damian Calabresi <damiancalabresi@gmail.com>
2015-11-29 03:11:38 -0300

    Agregué Swagger para auto-documentar los servicios REST

--- src/main/java/com/dcalabresi/momentous/back/Application.java
+++ src/main/java/com/dcalabresi/momentous/back/Application.java
@@ -1,11 +1,13 @@
 package com.dcalabresi.momentous.back;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan
+@EnableSwagger
 public class Application {
 
     public static void main(String[] args) {
